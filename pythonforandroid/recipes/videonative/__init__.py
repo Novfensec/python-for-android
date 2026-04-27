5d16e29a2482561c4f083e4f345e80141714014f
Kartavya Shukla <87070473+Novfensec@users.noreply.github.com>
2026-04-27 16:14:23 +0530

    Update __init__.py

--- pythonforandroid/recipes/videonative/__init__.py
+++ pythonforandroid/recipes/videonative/__init__.py
@@ -1,23 +1,31 @@
 from pythonforandroid.recipe import PyProjectRecipe
-
+from pythonforandroid.util import join_path
 
 class VideoNativeRecipe(PyProjectRecipe):
+
     version = '1.0.0'
     url = 'https://github.com/Novfensec/VideoNative/archive/main.zip'
     name = 'videonative'
+    site_packages_name = 'videonative'
     hostpython_prerequisites = ['scikit-build-core', 'pybind11', 'cmake', 'ninja']
     depends = ['python3', 'ffmpeg']
 
     def get_recipe_env(self, arch, **kwargs):
         env = super().get_recipe_env(arch, **kwargs)
+
         ffmpeg_recipe = self.get_recipe('ffmpeg', self.ctx)
         ffmpeg_build_dir = ffmpeg_recipe.get_build_dir(arch.arch)
-        
+
         env['SKBUILD_STRICT_CONFIG'] = 'false'
+
+        toolchain_file = join_path(self.ctx.ndk_dir, 'build', 'cmake', 'android.toolchain.cmake')
+
         env['SKBUILD_CMAKE_ARGS'] = (
-            "-DCMAKE_SYSTEM_NAME=Android;"
-            f"-DANDROID_FFMPEG_INCLUDE={ffmpeg_build_dir}/include;"
-            f"-DANDROID_FFMPEG_LIB={ffmpeg_build_dir}/lib;"
+            f"-DCMAKE_TOOLCHAIN_FILE={toolchain_file};"
+            f"-DANDROID_ABI={arch.arch};"
+            f"-DANDROID_PLATFORM=android-{self.ctx.ndk_api};"
+            f"-DANDROID_FFMPEG_INCLUDE={join_path(ffmpeg_build_dir, 'include')};"
+            f"-DANDROID_FFMPEG_LIB={join_path(ffmpeg_build_dir, 'lib')};"
         )
 
         return env
